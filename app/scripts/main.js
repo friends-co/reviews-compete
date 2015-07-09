ded9fc25b068696c442af1277b143ab271d10275
Joshua Tuscan <hello@joshuatuscan.com>
2015-07-09 00:14:12 -0700

    working pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,23 @@
 // reviews competitive spectrum
 
+'use strict';
 
 $( document ).ready(function() {
+
+    if ($("body").hasClass('intro') == false) {
+
+        var imgWidth = $('.framebody .images img:nth-child(2)').width();
+        var imgNum = $('.framebody .images img').length + 1;
+
+        var total = ((imgNum * imgWidth) + (imgNum * 50)) + 50;
+
+        console.log(imgNum);
+        console.log(imgWidth);
+        console.log(total);
+
+        $(".framebody .images").css('width', total);
+
+    }
 
     $('.sitenav a').on('click', function(e) {
         e.preventDefault();
@@ -11,6 +27,6 @@ $( document ).ready(function() {
 
         $('#contentFrame').attr('src', dest);
 
-    })
-  
+    });
 });
+
